f0eb053e535c58be54fbfea735690104b7472661
Egor <fomin0596@gmail.com>
2022-06-20 19:31:07 +0300

    added program

---
+++ task_3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class task_3{
+    public static void main(String[] args) {
+        
+        CheckIsDigit check = new CheckIsDigit();
+        Scanner console = new Scanner(System.in);
+        String name = console.nextLine();
+
+        if (check.isDigit(name) == true) {
+            System.out.println(triangle(Integer.parseInt(name)));
+        } else {
+            System.out.println("Введите целые числа");
+        }
+    }
+
+    public static int triangle(int n) {
+
+        if (n == 1)
+            return 1;
+        else
+            return (n + triangle(n - 1));
+
+    }
+}
